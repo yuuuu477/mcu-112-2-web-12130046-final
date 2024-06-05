0876f1db38702b7dfd9e4d60ef3010b4d04b033e
tiffany <chihyuu2547>
2024-06-05 18:55:33 +0800

    feat(product): 建立產品資料模型

---
+++ src/app/model/product.ts
@@ -0,0 +1,22 @@
+export class Product {
+  constructor(initData?: Partial<Product>) {
+    Object.assign(this, initData);
+    this.createDate = new Date();
+  }
+
+  id!: number;
+
+  name!: string;
+
+  authors!: string;
+
+  company!: string;
+
+  isShow!: boolean;
+
+  imgUrl!: string;
+
+  createDate!: Date;
+
+  price!: number;
+}
--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../model/product';
 import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
@@ -9,11 +10,14 @@ import { ProductCardComponent } from '../product-card/product-card.component';
   styleUrl: './product-card-list.component.css',
 })
 export class ProductCardListComponent {
-  productName = 'A產品';
-  authors = '作者A、作者B、作者C';
-  company = '博碩文化';
-  isShow = true;
-  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
-  createDate = new Date();
-  price = 1580.0;
+  product = new Product({
+    id: 1,
+    name: '書籍 A',
+    authors: '作者A、作者B、作者C',
+    company: '博碩文化',
+    isShow: true,
+    imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    createDate: new Date(),
+    price: 1580.0,
+  });
 }
