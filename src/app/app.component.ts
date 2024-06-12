d10511e06a24240cdeb5dae50304fffb54975d5f
tiffany <chihyuu2547>
2024-06-12 16:17:33 +0800

    feat(prodcut): 顯示無產品資料訊息實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,6 +52,55 @@ export class AppComponent {
       price: 1580.0,
     }),
   ];
+
+  onSetNoData(): void {
+    this.products = [];
+  }
+
+  onSetHasData(): void {
+    this.products = [
+      new Product({
+        id: 1,
+        name: 'A產品',
+        authors: '作者A、作者B、作者C',
+        company: '博碩文化',
+        isShow: true,
+        imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        createDate: new Date(),
+        price: 1580.0,
+      }),
+      new Product({
+        id: 2,
+        name: 'B產品',
+        authors: '作者A、作者B、作者C',
+        company: '博碩文化',
+        isShow: true,
+        imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        createDate: new Date(),
+        price: 1580.0,
+      }),
+      new Product({
+        id: 3,
+        name: 'C產品',
+        authors: '作者A、作者B、作者C',
+        company: '博碩文化',
+        isShow: true,
+        imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        createDate: new Date(),
+        price: 1580.0,
+      }),
+      new Product({
+        id: 4,
+        name: 'D產品',
+        authors: '作者A、作者B、作者C',
+        company: '博碩文化',
+        isShow: true,
+        imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        createDate: new Date(),
+        price: 1580.0,
+      }),
+    ];
+  }
 }
 /*productName = 'A產品';
   authors = '作者A、作者B、作者C';
