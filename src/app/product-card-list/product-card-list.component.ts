7c3c5a2e2f4d07bb465724a31d0252ca0165aa3d
tiffany <chihyuu2547>
2024-06-12 16:11:04 +0800

    feat(product): 依產品清單顥示實作

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../model/product';
 import { ProductCardComponent } from '../product-card/product-card.component';
 
@@ -10,14 +10,6 @@ import { ProductCardComponent } from '../product-card/product-card.component';
   styleUrl: './product-card-list.component.css',
 })
 export class ProductCardListComponent {
-  product = new Product({
-    id: 1,
-    name: '書籍 A',
-    authors: '作者A、作者B、作者C',
-    company: '博碩文化',
-    isShow: true,
-    imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-    createDate: new Date(),
-    price: 1580.0,
-  });
+  @Input({ required: true })
+  products!: Product[];
 }
