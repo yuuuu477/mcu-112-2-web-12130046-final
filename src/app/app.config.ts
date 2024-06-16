7383928618a6663e924cd3cfd82f3ba8ce4dd004
tiffany <chihyuu2547>
2024-06-16 10:32:45 +0800

    chore(db): 設定 json-server 套件

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-
+import { ProductRemoteService } from './services/product-remote.service';
+import { ProductService } from './services/product.service';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(),
+    { provide: ProductService, useClass: ProductRemoteService },
+  ],
 };
--- src/app/services/product-remote.service.ts
+++ src/app/services/product-remote.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../model/product';
@@ -12,21 +12,7 @@ export class ProductRemoteService extends ProductService {
 
   private readonly httpClient = inject(HttpClient);
 
-  override getList(
-    name: string | undefined,
-    pageIndex: number,
-    pageSize: number
-  ): Observable<Product[]> {
-    const query: { [key: string]: string | number } = {
-      _page: pageIndex,
-      _limit: pageSize,
-    };
-    if (name) query['name'] = name;
-    const params = new HttpParams({ fromObject: query });
-    return this.httpClient.get<Product[]>(this.url, { params });
-  }
-
-  override remove(productId: number): Observable<Product> {
-    return this.httpClient.delete<Product>(`${this.url}/${productId}`);
+  override getList(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.url);
   }
 }
